9f9b1dd45ed06d22b65fcc0cbf7dbbf9952eacef
markvong-o <83755325+markvong-o@users.noreply.github.com>
2022-03-06 23:46:21 -0800

    Edited the admin view portal

--- custom-login/src/views/AdminView.jsx
+++ custom-login/src/views/AdminView.jsx
@@ -10,6 +10,10 @@ const AdminView = () => {
   const [users, setUsers] = useState([]);
   const [groups, setGroups] = useState([]);
   const [apps, setApps] = useState([]);
+
+  const [currGroups, setCurrGroups] = useState([]);
+  const [currApps, setCurrApps] = useState([]);
+
   const [currUser, setCurrUser] = useState(null);
 
   const BASENAME = process.env.PUBLIC_URL || '';
@@ -72,13 +76,20 @@ const AdminView = () => {
     }
   }, [authState, oktaAuth]); // Update if authState changes
 
-
   const getResource = async (method, user, resource) => {
     const tokens = await localOktaAuth.token.getWithoutPrompt();
     const accessToken = tokens.tokens.accessToken.accessToken;
+    // let url =
+    //   resource === 'GROUPS'
+    //     ? `${usersUrl}/${user}/groups`
+    //     : `${usersUrl}/${user}/appLinks`;
     let url =
-      resource === 'GROUPS'
+      resource === 'GROUPS' && user == ''
+        ? `${groupsUrl}`
+        : resource === 'GROUPS' && user != ''
         ? `${usersUrl}/${user}/groups`
+        : resource === 'APPS' && user == ''
+        ? `${appsUrl}`
         : `${usersUrl}/${user}/appLinks`;
 
     const app_options = {
@@ -89,25 +100,35 @@ const AdminView = () => {
     };
     const resp = await fetch(url, app_options);
     const json = await resp.json();
-    switch (resource) {
-      case 'GROUPS':
-        setGroups(json);
-      case 'APPS':
-        setApps(json);
-    }
+    return json;
+    // switch (resource) {
+    //   case 'GROUPS':
+    //     setGroups(json);
+    //   case 'APPS':
+    //     setApps(json);
+    // }
   };
 
-  const isActive = (e) => {
-    setCurrUser(e.target.innerText);
+  const isActive = async (e) => {
     setGroups([]);
     setApps([]);
-    getResource('GET', e.target.value, 'GROUPS');
-    getResource('GET', e.target.value, 'APPS');
+    setCurrApps([]);
+    setCurrGroups([]);
+    e.persist();
+    setCurrUser(e.target.innerText);
+    const allGroups = await getResource('GET', '', 'GROUPS');
+    const allApps = await getResource('GET', '', 'APPS');
+    setGroups(allGroups);
+    setApps(allApps);
+    const currGroups = await getResource('GET', e.target.value, 'GROUPS');
+    const currApps = await getResource('GET', e.target.value, 'APPS');
+    setCurrApps(currApps);
+    setCurrGroups(currGroups);
   };
 
   const handleHidden = (e) => {
-    console.log(e.target)
-  }
+    console.log(e.target);
+  };
 
   return (
     <div id="admin-main-container">
@@ -124,7 +145,8 @@ const AdminView = () => {
                 value={user.id}
                 // animated="fade"
                 // inverted color="brown"
-                basic color="black"
+                basic
+                color="black"
               >
                 {user.profile.login}
               </Button>
@@ -134,47 +156,109 @@ const AdminView = () => {
       )}
       {currUser && (
         <div id="curr-user-div">
-          <Header as="h2" color="black">{currUser}</Header>
-          {groups.length > 0 && (
-            <div>
-              <Header as="h3" color="blue">Groups</Header>
-              <Button.Group className="grp-btn-grp">
-                {groups.map((group) => (
-                  <Button
-                    id={group.id}
-                    key={group.id}
-                    className="user-btn"
-                    value={group.id}
-                    animated="fade"
-                    inverted color="blue"
-                  >
-                    <Button.Content visible>{group.profile.name}</Button.Content>
-                    <Button.Content  onClick={handleHidden} hidden id={group.id}>Remove</Button.Content>
-                  </Button>
-                ))}
-              </Button.Group>
-            </div>
-          )}
-          {apps.length > 0 && (
-            <div>
-              <Header as="h3" color="orange">Apps</Header>
-              <Button.Group className="apps-btn-grp">
-                {apps.map((app) => (
-                  <Button
-                    id={app.id}
-                    key={app.id}
-                    className="user-btn"
-                    value={app.id}
-                    animated="fade"
-                    inverted color="orange"
-                  >
-                    <Button.Content visible>{app.label}</Button.Content>
-                    <Button.Content hidden>Remove</Button.Content>
-                  </Button>
-                ))}
-              </Button.Group>
-            </div>
-          )}
+          <Header as="h2" color="black">
+            {currUser}
+          </Header>
+          <div id="user-info-container">
+            {groups.length > 0 && currGroups.length > 0 && (
+              <div id="groups-container">
+                <Header as="h3" color="blue">
+                  Groups
+                </Header>
+                <Button.Group className="grp-btn-grp">
+                  {groups.map((group) => {
+                    // console.log(currGroups);
+                    return currGroups.map((g) => g.id).includes(group.id) &&
+                      group.profile.name != 'Everyone' ? (
+                      <Button
+                        id={group.id}
+                        key={group.id}
+                        className="info-btn"
+                        value={group.id}
+                        animated="fade"
+                        color="blue"
+                      >
+                        <Button.Content visible>
+                          {group.profile.name}
+                        </Button.Content>
+                        <Button.Content
+                          onClick={handleHidden}
+                          hidden
+                          id={group.id}
+                        >
+                          Remove
+                        </Button.Content>
+                      </Button>
+                    ) : group.profile.name != 'Everyone' ? (
+                      <Button
+                        id={group.id}
+                        key={group.id}
+                        className="info-btn"
+                        value={group.id}
+                        animated="fade"
+                        inverted
+                        color="red"
+                      >
+                        <Button.Content visible>
+                          {group.profile.name}
+                        </Button.Content>
+                        <Button.Content
+                          onClick={handleHidden}
+                          hidden
+                          id={group.id}
+                        >
+                          Add
+                        </Button.Content>
+                      </Button>
+                    ) : (
+                      ''
+                    );
+                  })}
+                </Button.Group>
+              </div>
+            )}
+            {apps.length > 0 && currApps.length > 0 && (
+              <div id="apps-container">
+                <Header as="h3" color="orange">
+                  Apps
+                </Header>
+                <Button.Group className="apps-btn-grp">
+                  {apps.map((app) => {
+                    // console.log(console.log(apps, currApps));
+                    // console.log(currApps.map((a) => a.id).includes(app.id), app.id);
+                    return currApps
+                      .map((a) => a.appInstanceId)
+                      .includes(app.id) ? (
+                      <Button
+                        id={app.id}
+                        key={app.id}
+                        className="info-btn"
+                        value={app.id}
+                        animated="fade"
+                        color="orange"
+                      >
+                        <Button.Content visible>{app.label}</Button.Content>
+                        <Button.Content hidden>Remove</Button.Content>
+                      </Button>
+                    ) : (
+                      <Button
+                        id={app.id}
+                        key={app.id}
+                        className="info-btn"
+                        value={app.id}
+                        animated="fade"
+                        inverted
+                        color="red"
+                      >
+                        <Button.Content visible>{app.label}</Button.Content>
+                        <Button.Content hidden>Add</Button.Content>
+                      </Button>
+                    );
+                  })}
+                </Button.Group>
+              </div>
+            )}
+          </div>
         </div>
       )}
       {/* <h2>Users</h2>
