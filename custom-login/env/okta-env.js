a4894d7326f74c7e22eee2ce0af31ea4cb4f10f9
markvong-o <83755325+markvong-o@users.noreply.github.com>
2023-02-13 12:41:48 -0800

    Potential breaking change

--- custom-login/env/okta-env.js
+++ custom-login/env/okta-env.js
@@ -16,10 +16,11 @@ const dotenv = require('dotenv');
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
-const ROOT_DIR = path.resolve(__dirname, '../..');
+const ROOT_DIR = path.resolve(__dirname, '../');
+console.log(ROOT_DIR);
 
 // Read environment variables from "testenv". Override environment vars if they are already set.
-const TESTENV = path.resolve(ROOT_DIR, 'testenv-oie');
+const TESTENV = path.resolve(ROOT_DIR, '.env');
 
 // Multiple sets of environment variables can be stored in a file called "testenv.yml"
 const TESTENV_YAML = path.resolve(ROOT_DIR, 'testenv.yml');
