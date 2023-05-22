52bb8c8209c34def2d4e9bf0c9ded814b9feafed
markvong-o <83755325+markvong-o@users.noreply.github.com>
2023-05-22 08:49:46 -0700

    Updated postlogoutredirect

--- custom-login/src/Navbar.jsx
+++ custom-login/src/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = ({ setCorsErrorModalOpen }) => {
   // Regular logout
   const logout = async () => {
     try {
-      await oktaAuth.signOut({ postLogoutRedirectUri: url });
+      await oktaAuth.signOut({ postLogoutRedirectUri: "/" });
     } catch (err) {
       if (isCorsError(err)) {
         setCorsErrorModalOpen(true);
